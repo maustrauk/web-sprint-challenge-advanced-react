abd3e8e310942d332250ab852a121e1fa152af94
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-12-04 12:18:05 -0500

    Testing the Checkout Form

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,53 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
+
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+const testUser = {
+    firstName: "First",
+    lastName: "Last",
+    address: "my adress",
+    city: "my city",
+    state: "my state",
+    zip: "0000"
+};
 
-test("form shows success message on submit with form details", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
+    screen.getByText("Checkout Form");
+});
+
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />);
+    const firstName = screen.getByLabelText(/first name/i);
+    const lastName = screen.getByLabelText(/last name/i);
+    const address = screen.getByLabelText(/address/i);
+    const city = screen.getByLabelText(/city/i);
+    const state = screen.getByLabelText(/state/i);
+    const zip = screen.getByLabelText(/zip/i);
+
+
+    userEvent.type(firstName, testUser.firstName);
+    userEvent.type(lastName, testUser.lastName);
+    userEvent.type(address, testUser.address);
+    userEvent.type(city, testUser.city);
+    userEvent.type(state, testUser.state);
+    userEvent.type(zip, testUser.zip);
+
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    screen.getByTestId("successMessage");
+
+    const newName = screen.queryByText(`${testUser.firstName} ${testUser.lastName}`);
+    const newAddress = screen.queryByText(testUser.address);
+    const newLocation =screen.queryByText(`${testUser.city}, ${testUser.state} ${testUser.zip}`);
+
+    expect(newName).toBeInTheDocument();
+    expect(newAddress).toBeInTheDocument();
+    expect(newLocation).toBeInTheDocument();
+});
