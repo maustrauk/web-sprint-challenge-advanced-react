6b3ef1f97f4fec51195b10867d5f88a57c3b4eb2
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-12-04 11:43:09 -0500

    Checkout Form

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import useForm from '../hooks/useForm';
 
 const initialValue = {
   firstName: "",
@@ -14,12 +14,8 @@ const initialValue = {
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
-
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  
+  const [setShowSuccessMessage, values, handleChanges, showSuccessMessage] = useForm(initialValue);
 
   const handleSubmit = (e) => {
     e.preventDefault();
